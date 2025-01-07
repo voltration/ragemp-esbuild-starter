104f4256e41c43818ec304ec71fee2b6e2cf3775
Sander <69873514+voltration@users.noreply.github.com>
2025-01-07 14:49:14 +0100

    Add ability to exlude deletion of files or directories

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,10 +1,10 @@
 import { bold, green, white, yellow } from "colorette";
-import { rimraf } from "rimraf";
 import { buildServer } from "./server.build.mjs";
 import { buildSvelte } from "./svelte.build.mjs";
 import { buildConfig } from "./conf.build.mjs";
 import { buildReact } from "./react.build.mjs";
 import { buildVue } from "./vue.build.mjs";
+import { deleteAsync } from 'del';
 import fs from "node:fs";
 import path from "node:path";
 import toml from "toml";
@@ -13,12 +13,16 @@ const start = performance.now();
 const file = fs.readFileSync(path.resolve("./config.toml"), "utf-8");
 const parsed = toml.parse(file);
 
+// use ! to exclude deletion
+const exclusionList = [
+	"!dist/client_packages/game_resources",
+	"!dist/client_packages/package2",
+];
+
 export const prod = parsed.config.production;
 
 async function clean() {
-	await rimraf(".temp");
-	await rimraf("dist/packages");
-	await rimraf("dist/client_packages");
+	await deleteAsync([".temp/**", "dist/packages/**", "dist/client_packages/**", ...exclusionList]);
 }
 
 (async () => {
